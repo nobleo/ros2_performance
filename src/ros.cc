427bd4ded59b00bca9a975d5e203411f250d1486
MartinCornelis2 <51268547+MartinCornelis2@users.noreply.github.com>
2019-09-19 15:13:16 +0200

    Update ros.cc

--- src/ros.cc
+++ src/ros.cc
@@ -25,14 +25,14 @@ int main(int argc, char **argv) {
   std::vector<rclcpp::Subscription<String>::SharedPtr> sub_refs;
   std::vector<rclcpp::TimerBase::SharedPtr> timer_refs;
 
-  auto options = rclcpp::PublisherOptions().use_intra_process_comm(true);
+  auto options = rclcpp::PublisherOptions().use_intra_process_comms(true);
   for (int n = 0; n < create; ++n) {
     auto node = std::make_shared<rclcpp::Node>("node_" + std::to_string(n));
     node_refs.push_back(node);
     exec.add_node(node);
 
-    auto pub1 = node->create_publisher<String>("topic_" + std::to_string(n), qos, options);
-    auto pub2 = node->create_publisher<String>("topic_" + std::to_string(n + create), qos, options);
+    auto pub1 = node->create_publisher<String>("topic_" + std::to_string(n), qos);
+    auto pub2 = node->create_publisher<String>("topic_" + std::to_string(n + create), qos);
     auto timer = node->create_wall_timer(interval, [=]() {
       String msg;
       msg.data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
